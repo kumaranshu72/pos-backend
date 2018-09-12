17e0e91fc63a8b7a873ddd24cee23ba15e52eb36
Anshu Kumar <anshu.kumar@ebizontek.com>
2018-09-12 22:43:39 +0530

    search rest api

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -12,10 +12,19 @@ class ItemController < ApplicationController
           hashtag = HashTag.create(name: t)
           hasttagrelation = ItemTagRelationship.create(item_id: Item.last.id,tag_id: HashTag.last.id)
         end
-        render status: 200, json: {Status: "Item created Successfully"}.to_json
       }
+      render status: 200, json: {Status: "Item created Successfully"}.to_json
     else
       render status: 500, json: {Status: "Bad Request"}.to_json
+    end
+  end
+
+  def search
+    result = Item.find_by_sql(["SELECT * FROM items WHERE id = (SELECT items.id FROM items LEFT JOIN item_tag_relationships ON item_tag_relationships.item_id = items.id JOIN hash_tags ON hash_tags.id = item_tag_relationships.tag_id WHERE hash_tags.name = ?)",params[:key]])
+    if result.any?
+      render status: 200, json: {Status: result}.to_json
+    else
+      render status: 200, json: {Status: "Resullt not found"}.to_json
     end
   end
 
