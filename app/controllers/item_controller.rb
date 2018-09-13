a7a3f6894729d6252df174a14b6936e0c75a5872
Anshu <anshu.kumar@ebizontek.com>
2018-09-13 11:07:45 +0530

    bill generation

--- app/controllers/item_controller.rb
+++ app/controllers/item_controller.rb
@@ -1,5 +1,6 @@
 class ItemController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :allow_cross_domain_ajax
 
   def create
     @item = Item.new(item_params)
@@ -19,8 +20,45 @@ class ItemController < ApplicationController
     end
   end
 
+  def search
+    result = Item.find_by_sql(["SELECT * FROM items WHERE id = (SELECT items.id FROM items LEFT JOIN item_tag_relationships ON item_tag_relationships.item_id = items.id JOIN hash_tags ON hash_tags.id = item_tag_relationships.tag_id WHERE hash_tags.name = ?)",params[:key]])
+    if result.any?
+      render status: 200, json: {Status: result}.to_json
+    else
+      render status: 200, json: {Status: "Resullt not found"}.to_json
+    end
+  end
+
+  def generate_bill
+    bill_amt = 0
+    items_array = []
+    params[:items].each { |t|
+      item = Item.find_by(id: t[:item_id])
+      bill_amt += item[:price]*t[:qty].to_f
+      item_hash = {:item_id=> t[:item_id],:unit_price=> item[:price], :qty=> t[:qty]}
+      items_array.push(item_hash)
+    }
+    svc_charge = bill_amt * 0.15
+    total = svc_charge + bill_amt
+    bill = Bill.create(bill_amount: bill_amt,service_charge: svc_charge,grand_total: total)
+    if bill
+      items_array.each { |i|
+        bill_item = BillItem.create(bill_id: Bill.last.id,item_id: i[:item_id],item_qty: i[:qty],unit_price: i[:unit_price])
+    }
+       render status: 200,json: {Message: "Bill Generated Sucessfully"}.to_json
+    else
+      render status: 200,json: {Message: "Something somewhere went terribly wrong1"}.to_json
+    end
+
+  end
+
   private
      def item_params
        params.require(:item).permit(:name, :price, :hashtag)
      end
+
+     def allow_cross_domain_ajax
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Request-Method'] = 'GET ,POST'
+  end
 end
